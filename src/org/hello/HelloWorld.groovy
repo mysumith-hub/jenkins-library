ba9dca1c2b23c116521ba56ee31e825040c4d002
Sumith Boddu <mysumith@gmail.com>
2020-04-16 14:18:27 -0700

    Testing script

--- src/org/hello/HelloWorld.groovy
+++ src/org/hello/HelloWorld.groovy
@@ -1,14 +1,18 @@
 package org.hello;
 
 class HelloWorld{
-
+  def script;
+  HelloWorld(script){
+    System.out.println("HelloWorld");
+    this.script = script;
+  }
+  def print(){
+    script.sh "echo 'script message Hello!'"
+    System.out.println("HelloWorld");
+  }
   static void main(String[] args){
     System.out.println("Hello Static World ");
   }
-  HelloWorld(){
-    System.out.println("HelloWorld");
-  }
-  def print(){
-    System.out.println("HelloWorld");
-  }
+
+
 }
