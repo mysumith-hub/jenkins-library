d6f177ae52925689b69bfc73c87d23e721e1e1e6
Sumith Boddu <mysumith@gmail.com>
2020-04-16 13:49:43 -0700

    Hello world

---
+++ src/org/hello/HelloWorld.groovy
@@ -0,0 +1,14 @@
+package org.hello;
+
+class HelloWorld{
+
+  static void main(String[] args){
+    System.out.println("Hello Static World ");
+  }
+  HelloWorld(){
+    System.out.println("HelloWorld");
+  }
+  def print(){
+    System.out.println("HelloWorld");
+  }
+}
